1c7f7af32334f0f5addde58afed5c40f71e6d4ad
Arya-Akshat <akshat.arya13@gmail.com>
2026-04-18 02:43:35 +0530

    Normalize API base URL to avoid double-slash endpoints

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -1,4 +1,4 @@
-const API_BASE = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080";
+const API_BASE = (process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080").replace(/\/+$/, "");
 
 const parseError = async (res) => {
   const data = await res.json().catch(() => ({}));
